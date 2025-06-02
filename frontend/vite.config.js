a4b19ec9e57a9c39196afa186dafdb991bcded43
Saisamarth Udikeri <saisamarthudikeri@gmail.com>
2025-06-02 20:46:37 +0530

    Test 1.1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      'localhost',
+      'lms.saisamarth.duckdns.org'
+    ],
+    watch: {
+      usePolling: true
+    }
+  }
 })
